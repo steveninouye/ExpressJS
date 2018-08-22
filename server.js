83fa96198c1773367958dfa20973c1387caf4d5e
Steven Inouye <steveninouye@msn.com>
2018-08-22 09:05:41 -1000

    completed required portion of assignment

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req,res) => {
-    res.send('Hellow from the web server side...')
+app.use((req,res,next) => {
+    console.log(req.url);
+    next();    
 })
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello from the web server side...');
+});
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
